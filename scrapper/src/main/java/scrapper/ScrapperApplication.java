a8a335c99dd65138ed44d171839f61a3563c24ad
mestreltsov <mestreltsov@edu.hse.ru>
2024-02-16 11:50:20 +0300

    task 4: implement Scheduler class

--- scrapper/src/main/java/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/scrapper/ScrapperApplication.java
@@ -4,21 +4,18 @@ import configuration.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import scrapper.client.GithubClient;
-import scrapper.client.StackoverflowClient;
-import scrapper.client.dto.GithubServiceUnitResponse;
-
-import java.util.Arrays;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
+@EnableScheduling
 public class ScrapperApplication {
 
     public static void main(String[] args) {
         var ctx = SpringApplication.run(ScrapperApplication.class, args);
         ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
-        System.out.println(Arrays.toString(ctx.getBean(GithubClient.class).getRepositoryInfo("mmstreltsov/java_course_hws")));
-        System.out.println(ctx.getBean(StackoverflowClient.class).getInfo("47373889"));
+//        System.out.println(Arrays.toString(ctx.getBean(GithubClient.class).getRepositoryInfo("mmstreltsov/java_course_hws")));
+//        System.out.println(ctx.getBean(StackoverflowClient.class).getInfo("47373889"));
 
         System.out.println(config);
     }
