56623b0469b69b2887070922ed2630bd68b6e0a5
mestreltsov <mestreltsov@edu.hse.ru>
2024-02-21 00:30:49 +0300

    minor changes: remove excess code (bad refactoring)

--- bot/src/main/java/bot/client/impl/ScrapperClientImpl.java
+++ bot/src/main/java/bot/client/impl/ScrapperClientImpl.java
@@ -2,22 +2,14 @@ package bot.client.impl;
 
 import bot.client.ScrapperClient;
 import bot.client.dto.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClient.RequestHeadersSpec;
-import org.springframework.web.reactive.function.client.WebClient.RequestHeadersUriSpec;
 
 import java.net.URI;
-import java.util.Objects;
-
-import static org.springframework.web.reactive.function.client.WebClient.RequestBodySpec;
-import static org.springframework.web.reactive.function.client.WebClient.ResponseSpec;
 
 @Service
 @AllArgsConstructor
@@ -112,64 +104,6 @@ public class ScrapperClientImpl implements ScrapperClient {
         handleBadResponse(response);
 
         return response.getBody();
-    }
-
-    @Builder
-    private record MakeRequest(@NotNull String path, Object[] pathVariables, String[] headerKeys,
-                               String[] headerValues, Object bodyValue, @NotNull Class<?> responseClass) {
-        public MakeRequest {
-            if (pathVariables == null) {
-                pathVariables = new Object[]{};
-            }
-            if (Objects.isNull(path) || Objects.isNull(responseClass)) {
-                throw new IllegalArgumentException("this fields can not be null:  path, response class");
-            }
-        }
-    }
-
-    private ResponseEntity<?> makePostRequest(MakeRequest makeRequest) {
-        RequestBodySpec part1 = webClient.post()
-                .uri(makeRequest.path, makeRequest.pathVariables)
-                .accept(MediaType.APPLICATION_JSON);
-
-        for (int i = 0; i < makeRequest.headerKeys.length; ++i) {
-            part1 = part1.header(makeRequest.headerKeys[i], makeRequest.headerValues[i]);
-        }
-
-        ResponseSpec retrieve;
-        if (makeRequest.bodyValue != null) {
-            retrieve = part1.bodyValue(makeRequest.bodyValue).retrieve();
-        } else {
-            retrieve = part1.retrieve();
-        }
-
-        return retrieve.onStatus(HttpStatusCode::is4xxClientError, r -> r.bodyToMono(ApiErrorResponse.class))
-                .toEntity(makeRequest.responseClass)
-                .block();
-    }
-
-    private ResponseEntity<?> makeGetRequest(MakeRequest makeRequest) {
-        return makeGetOrDeleteRequest(webClient.get(), makeRequest);
-    }
-
-    private ResponseEntity<?> makeDeleteRequest(MakeRequest makeRequest) {
-        return makeGetOrDeleteRequest(webClient.delete(), makeRequest);
-    }
-
-    private ResponseEntity<?> makeGetOrDeleteRequest(RequestHeadersUriSpec<?> requestHeadersUriSpec, MakeRequest makeRequest) {
-        RequestHeadersSpec<?> part1 = requestHeadersUriSpec
-                .uri(makeRequest.path, makeRequest.pathVariables)
-                .accept(MediaType.APPLICATION_JSON);
-
-        for (int i = 0; i < makeRequest.headerKeys.length; ++i) {
-            part1 = part1.header(makeRequest.headerKeys[i], makeRequest.headerValues[i]);
-        }
-
-        return part1
-                .retrieve()
-                .onStatus(HttpStatusCode::is4xxClientError, r -> r.bodyToMono(ApiErrorResponse.class))
-                .toEntity(makeRequest.responseClass)
-                .block();
     }
 
     private void handleBadResponse(ResponseEntity<?> response) {
